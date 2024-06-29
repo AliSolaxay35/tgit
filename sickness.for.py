dcfb4a9484fbd35fef37a423cfe4cb6dc9056013
AliSolaxay <alibavaria1860@gmail.com>
2024-06-29 23:12:19 +0330

    add sickness.for.py

---
+++ sickness.for.py
@@ -0,0 +1,9 @@
+sickness = {'infection', 'anemia', 'cancer'}
+
+count = 0
+for patients in sickness:    
+    print ('your sick')
+    count = count + 1
+    
+    print ('i said your sick', count, 'take the disease seriously')
+    
